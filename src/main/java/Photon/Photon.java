0d495988f57300767a20042e452595c0719c1901
zc <francislovem@gmail.com>
2018-02-13 21:22:37 +0800

    work

--- src/main/java/Photon/Photon.java
+++ src/main/java/Photon/Photon.java
@@ -1,98 +1,169 @@
 package Photon;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Photon extends Application{
+    private Scene scene = null;
     private StackPane root = null;
     private ImageView imgView = null;
+    private Text startupTips = null;
     private File[] fileList = null;
-    private ScrollPane scrollPane = null;
+    private File file = null;
+    private double imgWidth = 0;
+    private double imgHeight = 0;
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle(Settings.appName);
-        primaryStage.setResizable(Settings.windowResizable);
-        primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
-                Settings.width = number.intValue();
-            }
-        });
-        primaryStage.heightProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
-                Settings.height = number2.intValue();
-            }
-        });
-        primaryStage.setScene(new Scene(createScene()));
+        primaryStage.setTitle(Settings.APP_NAME);
+        primaryStage.setResizable(Settings.WINDOW_RESIZABLE);
+        scene = new Scene(createScene());
+        scene.setOnKeyPressed(k -> {
+                    if (k.getCode() == KeyCode.R) {
+                        revealImageInFinder();
+                    }
+                });
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     private void createPane() {
         root = new StackPane();
-        root.setOnMouseClicked((me) -> btnClicked(me));
         root.setAlignment(Pos.CENTER);
 
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        Settings.width = gd.getDisplayMode().getWidth();
-        Settings.height = gd.getDisplayMode().getHeight();
+        int tmpWidth = gd.getDisplayMode().getWidth();
+        int tmpHeight = gd.getDisplayMode().getHeight();
 
-        root.setPrefSize(Settings.width, Settings.height);
-        Rectangle rect = new Rectangle(Settings.width, Settings.height, Color.BLACK);
+        root.setPrefSize(tmpWidth, tmpHeight);
+        Rectangle rect = new Rectangle(tmpWidth, tmpHeight, Settings.BACKGROUND_COLOR);
         root.getChildren().add(rect);
+
+        startupTips = new Text(Settings.STARTUP_TIPS);
+        startupTips.setFill(Color.WHITE);
+        startupTips.setFont(new Font("Helvetica", 25));
+        root.getChildren().add(startupTips);
 
         imgView = new ImageView();
         imgView.setPreserveRatio(true);
-        scrollPane = new ScrollPane();
+        imgView.fitWidthProperty().bind(root.widthProperty());
+        imgView.fitHeightProperty().bind(root.heightProperty());
+
+        ObjectProperty<Point2D> mouseDown = new SimpleObjectProperty<>();
+
+        imgView.setOnMousePressed(e -> {
+            Point2D mousePress = imageViewToImage(imgView, new Point2D(e.getX(), e.getY()));
+            mouseDown.set(mousePress);
+        });
+
+        imgView.setOnMouseDragged(e -> {
+            Point2D dragPoint = imageViewToImage(imgView, new Point2D(e.getX(), e.getY()));
+            shift(imgView, dragPoint.subtract(mouseDown.get()));
+            mouseDown.set(imageViewToImage(imgView, new Point2D(e.getX(), e.getY())));
+        });
+
+        imgView.setOnScroll(e -> {
+            double delta = e.getDeltaY();
+            Rectangle2D viewport = imgView.getViewport();
+
+            double scale = clamp(Math.pow(1.01, delta),
+
+                    // don't scale so we're zoomed in to fewer than MIN_PIXELS in any direction:
+                    Math.min(Settings.MIN_PIXELS / viewport.getWidth(), Settings.MIN_PIXELS / viewport.getHeight()),
+
+                    // don't scale so that we're bigger than image dimensions:
+                    Math.max(imgWidth / viewport.getWidth(), imgHeight / viewport.getHeight())
+
+            );
+
+            Point2D mouse = imageViewToImage(imgView, new Point2D(e.getX(), e.getY()));
+
+            double newWidth = viewport.getWidth() * scale;
+            double newHeight = viewport.getHeight() * scale;
+
+            // To keep the visual point under the mouse from moving, we need
+            // (x - newViewportMinX) / (x - currentViewportMinX) = scale
+            // where x is the mouse X coordinate in the image
+
+            // solving this for newViewportMinX gives
+
+            // newViewportMinX = x - (x - currentViewportMinX) * scale
+
+            // we then clamp this value so the image never scrolls out
+            // of the imageview:
+
+            double newMinX = clamp(mouse.getX() - (mouse.getX() - viewport.getMinX()) * scale,
+                    0, imgWidth - newWidth);
+            double newMinY = clamp(mouse.getY() - (mouse.getY() - viewport.getMinY()) * scale,
+                    0, imgHeight - newHeight);
+
+            imgView.setViewport(new Rectangle2D(newMinX, newMinY, newWidth, newHeight));
+        });
+
+        root.setOnMouseClicked(e -> {
+            if (e.getButton().name().equals("SECONDARY")) {
+                btnClicked(e);
+            }
+//            if (e.getClickCount() == 1 && e.getButton().name().equals("SECONDARY")) {
+//                btnClicked(e);
+//            }
+//            else if (e.getClickCount() == 2 && e.getButton().name().equals("PRIMARY")) {
+//                reset(imgView, imgWidth, imgHeight);
+//            }
+        });;
+
         root.getChildren().add(imgView);
     }
 
     private void btnClicked(MouseEvent me) {
         if (fileList == null) {
+            // get files at firstA
             DirectoryChooser directoryChooser = new DirectoryChooser();
 
             File directory = directoryChooser.showDialog(null);
-
-            fileList = directory.listFiles(new FileFilter() {
-                @Override
-                public boolean accept(File pathname) {
-                    if (pathname.isDirectory()) {
-                        return false;
-                    } else {
-                        return true;
-                    }
-                }
-            });
+            if (directory == null) {
+                return;
+            } else {
+                fileList = getFiles(directory);
+            }
 
             if (fileList.length == 0) {
                 fileList = null;
+                startupTips.setVisible(true);
                 return;
+            } else {
+                startupTips.setVisible(false);
             }
         }
 
-        File file = fileList[new Random().nextInt(fileList.length)];
+        file = fileList[new Random().nextInt(fileList.length)];
+
         Image image = null;
         try {
             image = new Image(new FileInputStream(file));
@@ -100,17 +171,54 @@ public class Photon extends Application{
             return;
         }
 
-        imgView.setFitWidth(Settings.width);
-        imgView.setFitHeight(Settings.height);
+        imgWidth = image.getWidth();
+        imgHeight = image.getHeight();
         imgView.setImage(image);
+        reset(imgView, imgWidth, imgHeight);
+    }
+
+    private void revealImageInFinder() {
+        if (file == null || !file.exists()) {
+            return;
+        }
+
+        try {
+            Runtime.getRuntime().exec("open -R " + file.getPath());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private File[] getFiles(File dir) {
+        if (Settings.ADD_IMAGE_RECURSIVELY) {
+            ArrayList<File> files = new ArrayList<>();
+            try {
+                Files.walk(Paths.get(dir.toURI()))
+                        .filter(Files::isRegularFile)
+                        .forEach(f -> files.add(f.toFile()));
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+            return files.toArray(new File[files.size()]);
+        } else {
+            return dir.listFiles(pathname -> {
+                if (pathname.isDirectory()) {
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        }
     }
 
     private void reset(ImageView imageView, double width, double height) {
         imageView.setViewport(new Rectangle2D(0, 0, width, height));
     }
 
-    // shift the viewport of the imageView by the specified delta, clamping so
-    // the viewport does not move off the actual image:
+    /**
+     * shift the viewport of the imageView by the specified delta, clamping so
+     * the viewport does not move off the actual image:
+     */
     private void shift(ImageView imageView, Point2D delta) {
         Rectangle2D viewport = imageView.getViewport();
 
@@ -136,7 +244,9 @@ public class Photon extends Application{
         }
     }
 
-    // convert mouse coordinates in the imageView to coordinates in the actual image:
+    /**
+     * convert mouse coordinates in the imageView to coordinates in the actual image
+     */
     private Point2D imageViewToImage(ImageView imageView, Point2D imageViewCoordinates) {
         double xProportion = imageViewCoordinates.getX() / imageView.getBoundsInLocal().getWidth();
         double yProportion = imageViewCoordinates.getY() / imageView.getBoundsInLocal().getHeight();
--- src/main/java/Photon/Settings.java
+++ src/main/java/Photon/Settings.java
@@ -1,8 +1,16 @@
 package Photon;
 
+import javafx.scene.paint.Color;
+
 public class Settings {
-    public static final String appName = "Photon";
-    public static final boolean windowResizable = true;
-    public static int width = 0;
-    public static int height = 0;
+    public static final String APP_NAME = "Photon";
+    public static final String STARTUP_TIPS =   "Right Click  \t->\tOpen Folder / Switch to Next Photo\n\n" +
+                                                "Scroll       \t->\tZoom Photo\n\n" +
+                                                "Drag         \t->\tMove Photo\n\n" +
+                                                "Press 'R'    \t->\tShow in Finder/Explorer\n\n";
+                                                //"Double Click \t->\tReset Photo to Fit Window Size\n\n";
+    public static final boolean WINDOW_RESIZABLE = true;
+    public static final Color BACKGROUND_COLOR = Color.BLACK;
+    public static final boolean ADD_IMAGE_RECURSIVELY = true;
+    public static final int MIN_PIXELS = 10;
 }
